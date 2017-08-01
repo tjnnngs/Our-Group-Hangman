a37d0fda89328c8fa1efe4d903b03556e388f9d4
Anne <ameadows@k12.wv.us>
2017-07-31 22:14:17 -0400

    works put not pretty

--- app.rb
+++ app.rb
@@ -1,44 +1,54 @@
-def get_word?(secret_word) 
-	if secret_word.class == String
-		true
-	else
-		false
+#Hangman
+
+def get_word()
+	#creates an array of a secret words to choose from
+	wordbank = ["foobar", "tapioca", "wombat", "skylight", "lizard", "fox"]
+	#randomly chooses a word from thr wordbank for the game
+	secret_word_array = wordbank.shuffle.first.scan(/./)
+	#creates an empty array to build the guessed word
+	my_word_guess = []
+	secret_word_array.length.times do
+		my_word_guess << "_"
 	end
+	guess_a_letter(secret_word_array, my_word_guess) 
 end
 
-number_of_letters_in_secret_word = 0
-def get_secret_word_length(secret_word)
-	number_of_letters_in_secret_word = secret_word.length
-end
-
-def in_word?(secret_word, guess)
-	right_guesses = []
-	wrong_guesses = []
-	if secret_word.include? guess
-		right_guesses << guess
-		true
-	else
-		wrong_guesses << guess
-		false
-	end
-end 
-
-def determine_position(secret_word, guess)
-	found = false
-	my_guess = []
-	secret_word_array = []
-	secret_word.each_char do |letter|
-		secret_word_array << letter
-	end
-	counter = 0
-	secret_word_array.each do |letter|
-		if letter == guess
-			my_guess[counter] = guess
-			found = true
+def guess_a_letter(secret_word_array, my_word_guess)
+	solved = false
+	wrong_guesses = 0
+	wrong_letters = []
+	while solved == false && wrong_guesses < 10
+		puts "The secret word has #{secret_word_array.length} letters and currently looks like #{my_word_guess}"
+		puts "Guess a letter: "
+		guess = gets.chomp
+# 		if wrong_letters.include?(guess)
+# 			puts "You already guessed #{guess}!"
+# 		end
+		counter = 0
+		found = false
+		secret_word_array.each do |letter|
+			if letter == guess
+				my_word_guess[counter] = guess
+				found = true
+			end
+		counter = counter + 1
 		end
-	counter = counter + 1
-	end
-	p "The secret word is #{secret_word_array}"
-	p "My guess is #{my_guess}"
-	found	
+		if found == false
+			wrong_guesses = wrong_guesses + 1
+			wrong_letters << guess
+			puts "Letter not found; wrong guesses = #{wrong_guesses} including #{wrong_letters}"
+		end
+		if my_word_guess == secret_word_array
+			solved = true
+			puts "TA-DA! You guessed the secret word: #{secret_word_array}!"
+		end
+
+		if wrong_guesses == 10
+			puts "Too many wrong guesses. Try again!"
+		end
+	end	
 end
+
+
+get_word()
+
