174fd6c9361144e9361dabe07e1551cd233f13a0
Anne <ameadows@k12.wv.us>
2017-08-01 20:51:58 -0400

    added a list of available letters

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ def get_secret_word()
 	wordbank = ["foobar", "tapioca", "wombat", "skylight", "lizard", "fox"]
 	#randomly chooses a word from thr wordbank for the game
 	$secret_word_array = wordbank.shuffle.first.scan(/./)
+
 end
 
 def display_blanks(secret_word_array)
@@ -12,18 +13,24 @@ def display_blanks(secret_word_array)
 	$my_guess_array = []
 	$secret_word_array.length.times do
 		$my_guess_array << "_"
-	end
+	end	
+end
+def create_letterbox_hash()
+	$alphabet = [*?a..?z] #creates an array from the alphabet
+	position = (1..26).to_a #creates an array of the corresponding numbers of the alphabet letters
+	$letterbox = Hash[$alphabet.zip position]
 end
 
-def guess_a_letter(secret_word_array, my_guess_array, wrong_guesses, wrong_letters, solved)
+def guess_a_letter(secret_word_array, my_guess_array, alphabet, wrong_guesses, wrong_letters, solved)
 	puts "The secret word has #{$secret_word_array.length} letters and currently looks like #{$my_guess_array}"
-	puts "Guess a letter: "
+	puts "Pick a letter from #{$alphabet}"
 	counter = 0
 	found = false
 	guess = gets.chomp
 	$secret_word_array.each do |letter|
 		if letter == guess
 			$my_guess_array[counter] = guess
+			$alphabet[$letterbox[guess] - 1] = " "
 			found = true
 		end
 	counter = counter + 1
@@ -52,11 +59,12 @@ end
 def run_program()
 	get_secret_word()
 	display_blanks($secret_word_array)
+	create_letterbox_hash()
 	$solved = false
 	$wrong_guesses = 0
 	$wrong_letters = []
 	while $solved == false && $wrong_guesses < 10
-		guess_a_letter($secret_word_array, $my_guess_array, $wrong_guesses, $wrong_letters, $solved)
+		guess_a_letter($secret_word_array, $my_guess_array, $letterbox, $wrong_guesses, $wrong_letters, $solved)
 		you_lose($wrong_guesses, $secret_word_array)
 		you_win($secret_word_array, $my_guess_array, $solved)
 	end
